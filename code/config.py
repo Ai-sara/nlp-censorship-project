2073c8f011f962db0a57e548f06a2f335da136be
Ai_aisara <112186305+Ai-sara@users.noreply.github.com>
2026-04-27 20:57:01 +0500

    Remove sensitive files

--- code/config.py
+++
@@ -1,9 +0,0 @@
-# API Keys
-OPENROUTER_API_KEY = "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"
-
-# Models
-MODELS = {
-    "gemma": "google/gemma-3-4b-it:free",
-    "gpt_oss": "openai/gpt-oss-20b:free",
-    "llama": "meta-llama/llama-3.3-70b-instruct:free"
-}
